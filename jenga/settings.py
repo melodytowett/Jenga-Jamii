13caf1be0c92b3a9f10b5aaa211d55ca634cd70d
melodytowett <melodytowett99@gmail.com>
2022-07-25 11:03:20 +0300

    add databe models

--- jenga/settings.py
+++ jenga/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'jenga.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Jenga',
+        'USER':'postgres',
+        'PASSWORD':'Melo',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
